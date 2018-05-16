0133426c074c6623a5a49cf4d5914a59e52dbf06
Fleur Assendelft <fleurassendelft@hotmail.nl>
2018-05-16 21:08:09 +0200

    Added Nederlands

--- javascript/main.js
+++ javascript/main.js
@@ -8,6 +8,24 @@ function foto(x){
 	}
 }
 
+function greeting(){
+        var d = new Date();
+        var time = d.getHours();
+
+        if (time > 0 && time < 12){
+            document.getElementById('greeting').innerHTML = "Hi good morning, I'm Fleur!" ;
+        }
+        if(time >= 12 ){
+            document.getElementById('greeting').innerHTML = "Hi good afternoon, I'm Fleur!";
+        }
+        if(time >= 18 ){
+            document.getElementById('greeting').innerHTML = "Hi good evening, I'm Fleur!";
+        }
+    }
+
 var x = window.matchMedia("(max-width: 725px)")
 foto(x) // Call listener function at run time
 x.addListener(foto) // Attach listener function on state changes
+
+greeting()
+
